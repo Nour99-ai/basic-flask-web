31518882d3a40fbf5f5fe7972363f3ab5874645d
Nour <nm2012k@gmail.com>
2025-05-22 18:56:24 +0200

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
-@app.route('/heram', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
